75aeda5d32a92cff2c4bf108dc3d766849853fd4
JanaStepanchak <jana.stepanchak@gmail.com>
2017-12-26 16:01:59 +0200

    change2 section slide

--- src/js/what-they-says.js
+++ src/js/what-they-says.js
@@ -1,5 +1,6 @@
 document.getElementById('slid1-what-they-says').style.display = 'flex';
 document.getElementById('radio1-what-they-says').style.background = 'rgb(113, 246, 235)'
+radio = 1;
 for (var i = 2; i < 5; i++) {
     document.getElementById('slid' + i + '-what-they-says').style.display = 'none';
 }
@@ -20,6 +21,7 @@ document.getElementById('radio1-what-they-says').onclick = function () {
             document.getElementById('radio' + i + '-what-they-says').style.background = 'rgb(208, 208, 208)';
         }
     }
+    radio = 1;
 };
 document.getElementById('radio2-what-they-says').onclick = function () {
     this.style.background = 'rgb(113, 246, 235)';
@@ -31,6 +33,7 @@ document.getElementById('radio2-what-they-says').onclick = function () {
             document.getElementById('radio' + i + '-what-they-says').style.background = 'rgb(208, 208, 208)';
         }
     }
+    radio = 2;
 };
 document.getElementById('radio3-what-they-says').onclick = function () {
     this.style.background = 'rgb(113, 246, 235)';
@@ -42,6 +45,7 @@ document.getElementById('radio3-what-they-says').onclick = function () {
             document.getElementById('radio' + i + '-what-they-says').style.background = 'rgb(208, 208, 208)';
         }
     }
+    radio = 3;
 };
 document.getElementById('radio4-what-they-says').onclick = function () {
     this.style.background = 'rgb(113, 246, 235)';
@@ -53,4 +57,42 @@ document.getElementById('radio4-what-they-says').onclick = function () {
             document.getElementById('radio' + i + '-what-they-says').style.background = 'rgb(208, 208, 208)';
         }
     }
+    radio = 4;
 };
+
+
+//
+//function NextLeft(){
+//    radio = radio - 1;
+//    if(radio == 0){
+//    radio = 4;
+//    };
+//    document.getElementById('radio' + radio + '-what-they-says').style.background = 'rgb(113, 246, 235)';
+//    document.getElementById('slid' + radio + '-what-they-says').style.display = 'flex';
+//    for (var j = 1; j < 5; i++) {
+//        if (j != radio) {
+//            document.getElementById('slid' + j + '-what-they-says').style.display = 'none';
+//            document.getElementById('radio' + j + '-what-they-says').style.background = 'rgb(208, 208, 208)';
+//        }
+//    }
+//}
+
+
+
+
+
+//
+//function NextRight(){
+//    radio = radio + 1;
+//    if(radio == 5){
+//    radio = 1;
+//    };
+//    document.getElementById('radio' + radio + '-what-they-says').style.background = 'rgb(113, 246, 235)';
+//    document.getElementById('slid' + radio + '-what-they-says').style.display = 'flex';
+//    for (var j = 1; j < 5; i++) {
+//        if (j != radio) {
+//            document.getElementById('slid' + j + '-what-they-says').style.display = 'none';
+//            document.getElementById('radio' + j + '-what-they-says').style.background = 'rgb(208, 208, 208)';
+//        }
+//    }
+//}
